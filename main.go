c1a0e8a3291844fbfa46cd4dd61b04f3fdd89fa3
JoshDPT <jjewcu@gmail.com>
2023-08-22 22:18:17 -0400

    add typing game

--- main.go
+++ main.go
@@ -1,28 +1,82 @@
 package main
 
 import (
-	"fmt"
-	"github.com/rivo/tview"
-	"os"
+  "log"
+  "encoding/json"
+  "fmt"
+  "io"
+  "net/http"
+  "time"
 )
 
+type Quote struct {
+	Quote string `json:"q"`
+	Author string `json:"a"`
+}
+
 func main() {
-	app := tview.NewApplication()
+	quotes, err := getRandomQuote()
+	if err != nil {
+		log.Fatalf("Error fetching quote: %v", err)
+	}
 
-	// Create a text view
-	textView := tview.NewTextView().
-		SetText("Use arrow keys to move.\nPress 'q' to quit").
-		SetTextAlign(tview.AlignCenter).
-		SetDynamicColors(true)
+  quote := quotes[0]
 
-	// Create a grid layout and add the text view to it
-	grid := tview.NewGrid().
-		SetRows(0).
-		SetColumns(0).
-		AddItem(textView, 0, 0, 1, 1, 0, 0, true)
+	for {
+		fmt.Println("Type the following sentence:")
+		fmt.Println(quote.Quote)
 
-	if err := app.SetRoot(grid, true).Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		startTime := time.Now()
+
+		var userInput string
+		fmt.Print("Your input: ")
+		fmt.Scanln(&userInput)
+
+		endTime := time.Now()
+
+		typingTime := endTime.Sub(startTime).Seconds()
+		typingSpeed := float64(len(quote.Quote)) / typingTime // characters per second
+		accuracy := calculateAccuracy(quote.Quote, userInput) // percentage
+
+    fmt.Printf("Typing Speed: %.2f characters per second\n", typingSpeed)
+    fmt.Printf("Accuracy: %.2f%%\n", accuracy)
+    
+		fmt.Print("Do you want to play again? (y/n): ")
+		var playAgain string
+		fmt.Scanln(&playAgain)
+		if playAgain != "y" {
+			break
+		}
 	}
 }
 
+func calculateAccuracy(original, typed string) float64 {
+	correctCount := 0
+	for i := 0; i < len(original) && i < len(typed); i++ {
+		if original[i] == typed[i] {
+			correctCount++
+		}
+	}
+	return float64(correctCount) / float64(len(original)) * 100
+}
+
+func getRandomQuote() ([]Quote, error) {
+  client := http.Client{}
+  resp, err := client.Get("https://zenquotes.io/api/random")
+  if err != nil {
+    return nil, fmt.Errorf("failed to fetch quote: %w", err)
+  }
+  defer resp.Body.Close()
+
+  body, err := io.ReadAll(resp.Body)
+  if err != nil {
+    return nil, fmt.Errorf("failed to read response body: %w", err)
+  }
+
+  var quotes []Quote
+  if err := json.Unmarshal(body, &quotes); err != nil {
+    return nil, fmt.Errorf("failed to parse response body: %w", err)
+  }
+
+  return quotes, nil
+}
